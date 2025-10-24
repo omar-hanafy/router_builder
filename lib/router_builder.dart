a40e34ff585ca6b19574e2d81f966961a5fd22a5
omar-hanafy <omar_hanafy@icloud.com>
2025-10-24 16:36:44 +0300

    Update router_builder.dart

--- lib/router_builder.dart
+++ lib/router_builder.dart
@@ -1,4 +1,8 @@
+/// todo add docs
+library;
+
 export 'annotations/route.dart';
 export 'deeplink/deep_link_matcher.dart';
 export 'handlers/deep_link_handler.dart';
-export 'models/models.dart';
+export 'models/route_args.dart';
+export 'models/route_info.dart';
